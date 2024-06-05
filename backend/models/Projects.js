1dfa9b206812804e5aa5bd8788ec794ef533b991
wguadalupe <w.guadalupe78@gmail.com>
2024-06-05 19:14:41 -0400

    Completed project and task models

--- backend/models/Projects.js
+++ backend/models/Projects.js
@@ -1,20 +1,64 @@
+'use strict';
+
 const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     const Projects = sequelize.define('Projects', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
         description: {
             type: DataTypes.STRING,
             allowNull: false
           },
-          Date: {
-            type: DataTypes.RANGE(DataTypes.DATE),
+          startDate: { 
+            type: DataTypes.DATE,
             allowNull: false
           },
-          cost: {
+          endDate: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          price: {
             type: DataTypes.DECIMAL(10,2),
             allowNull: false
+          },
+          startDate: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          endDate: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          client_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+          price_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+          freelancer_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+              model: 'Users', // name of the table
+              key: 'id' // name of the column
+            }
           }
-        });
-        
-        module.exports = Projects;
+}, {});
+//associations
+Projects.associate = function(models) { 
+    Projects.belongsTo(models.Users, {foreignKey: 'client_id', as: 'client'});
+    Projects.belongsTo(models.Users, {foreignKey: 'freelancer_id', as: 'freelancer'});  
+    Projects.belongsTo(models.Prices, {foreignKey: 'price_id', as: 'price'});
+           
 
 };
+
+return Projects;
+
+};
+
+module.exports = Projects;
---
+++ backend/models/Tasks.js
@@ -0,0 +1,60 @@
+'use_state';
+
+const { DataTypes } = require('sequelize');
+module.exports = (sequelize) => {
+    const Tasks = sequelize.define('Tasks', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          startDate: { 
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          endDate: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          price: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false
+          },
+          startDate: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          client_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+          price_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+          freelancer_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+              model: 'Users', // name of the table
+              key: 'id' // name of the column
+            }
+          }
+}, {});
+//associations
+Tasks.associate = function(models) { 
+    Tasks.belongsTo(models.Users, {foreignKey: 'client_id', as: 'client'});
+    Tasks.belongsTo(models.Users, {foreignKey: 'freelancer_id', as: 'freelancer'});  
+    Tasks.belongsTo(models.Prices, {foreignKey: 'price_id', as: 'price'});
+           
+
+};
+
+return Tasks;
+
+};
+
+module.exports = Tasks;
