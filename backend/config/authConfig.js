e5d579190355f88438cfb88f0493768db458ae59
wguadalupe <w.guadalupe78@gmail.com>
2024-06-10 22:45:08 -0400

    Added 'authConfig' for secret key, added 'authMware' for middleware, and authController

---
+++ backend/config/authConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: SECRET_KEY //used for signing and verifying the tokens to ensure integrity and authenticity
+};
---
+++ backend/middleware/authMware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/authConfig');
+const dbase = require('../models');
+const User = dbase.user;
+
+const vToken = (req, res, next) => {
+    let token = req.headers['pl-access-token']; // 'pl-access-token' is the token naming convention for pro-lance
+    if (!token) {
+        return res.status(403).send({ message: 'Token error, please contact your system admin.' });
+    }
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Authentication failed!' });
+        }
+        req.user_id = decoded_id;
+        next();
+    });
+};
+
+module.exports = { vToken };
