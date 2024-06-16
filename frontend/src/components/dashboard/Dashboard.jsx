3281963247a8e0cbad81aa95b25898822b822a21
M4RKL4B0Y <markvlaboy@gmail.com>
2024-06-15 22:18:31 -0400

    test

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, Route, Routes } from 'react-router-dom';
 import "./Dashboard.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import Projects from './Projects';
+import Tasks from './Tasks';
+import Comments from './Comments';
+import RequestForm from '../forms/RequestForm';
 
 function Dashboard() {
   return (
@@ -12,19 +17,21 @@ function Dashboard() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <Nav.Link href="#dashboard">Dashboard</Nav.Link>
-                <Nav.Link href="#projects">Projects</Nav.Link>
-                <Nav.Link href="#prolancers">Prolancers</Nav.Link>
+                <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+                <Nav.Link as={Link} to="projects">Projects</Nav.Link>
+                <Nav.Link as={Link} to="tasks">Tasks</Nav.Link>
+                <Nav.Link as={Link} to="comments">Comments</Nav.Link>
+                <Nav.Link as={Link} to="newRequest">New Request</Nav.Link>
                 <Nav.Link href="#account">Account</Nav.Link>
-                <form class="form-inline my-2 my-lg-0">
+                <form className="form-inline my-2 my-lg-0">
                   <input
-                    class="form-control mr-sm-2"
+                    className="form-control mr-sm-2"
                     type="search"
                     placeholder="Search"
                     aria-label="Search"
                   />
                   <button
-                    class="btn btn-outline-success my-2 my-sm-0"
+                    className="btn btn-outline-success my-2 my-sm-0"
                     type="submit"
                   >
                     Search
@@ -37,7 +44,7 @@ function Dashboard() {
       </div>
 
       <div className="jumbotron">
-        <h1 className="display">Hey Kyle</h1>
+        <h1 className="display-4">Hey Kyle</h1>
         <p className="lead">
           Discover professionals in your area for your projects, tasks or
           errands.
@@ -51,8 +58,25 @@ function Dashboard() {
           Learn more
         </a>
       </div>
+
+      <div>
+        <Routes>
+          <Route path="projects" element={<Projects />} />
+          <Route path="tasks" element={<Tasks />} />
+          <Route path="comments" element={<Comments />} />
+          <Route path="newRequest" element={<RequestForm />} />
+        </Routes>
+      </div>
     </main>
   );
 }
 
 export default Dashboard;
+
+
+
+
+
+
+
+
